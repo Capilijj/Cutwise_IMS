fdaa5b574569936fa932e589af0fa8e64e6e06c4
BroisHi <Seijoroakashii@gmail.com>
2026-06-14 09:08:59 +0800

    feat : added integration for the sales sysystem and the delivery system

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080,
+    port: 8081,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 })
